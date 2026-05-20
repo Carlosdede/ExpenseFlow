b2d705bafea65b6d0d26312ac66dd616ddbaf66d
Carlos Andre <contato.carlosdede@gmail.com>
2026-05-20 01:09:18 -0300

    wip: ajusta serviços e repositórios de usuário e endereço

--- src/main/java/dev/carlosdede/expenseflow/address/service/AddressService.java
+++ src/main/java/dev/carlosdede/expenseflow/address/service/AddressService.java
@@ -1,6 +1,11 @@
 package dev.carlosdede.expenseflow.address.service;
 
+import dev.carlosdede.expenseflow.address.dto.AddressRequestDTO;
+import dev.carlosdede.expenseflow.address.dto.AddressResponseDTO;
+import dev.carlosdede.expenseflow.address.entity.AddressEntity;
+import dev.carlosdede.expenseflow.address.mapper.AddressMapper;
 import dev.carlosdede.expenseflow.address.repository.AddressRepository;
+import dev.carlosdede.expenseflow.user.dto.UserCreateRequestDTO;
 import org.springframework.stereotype.Service;
 
 
@@ -8,10 +13,23 @@ import org.springframework.stereotype.Service;
 public class AddressService {
 
     private final AddressRepository addressRepository;
+    private final AddressMapper addressMapper;
 
-    public AddressService(AddressRepository addressRepository){
+
+    public AddressService(AddressRepository addressRepository, AddressMapper addressMapper){
         this.addressRepository = addressRepository;
+        this.addressMapper = addressMapper;
     }
 
+    public AddressResponseDTO createAddress(UserCreateRequestDTO userCreateRequestDTO, AddressRequestDTO addressRequestDTO) {
+        if(userCreateRequestDTO.addressRequestDTO() != null){
+            AddressEntity address = addressMapper.toEntity(addressRequestDTO);
+            AddressEntity newAddress = addressRepository.save(address);
+
+            return addressMapper.toDTO(newAddress);
+
+        }
+        return null;
+    }
 
 }
--- src/main/java/dev/carlosdede/expenseflow/user/service/UserService.java
+++ src/main/java/dev/carlosdede/expenseflow/user/service/UserService.java
@@ -1,14 +1,38 @@
 package dev.carlosdede.expenseflow.user.service;
 
+import dev.carlosdede.expenseflow.address.dto.AddressRequestDTO;
+import dev.carlosdede.expenseflow.address.dto.AddressResponseDTO;
+import dev.carlosdede.expenseflow.address.entity.AddressEntity;
+import dev.carlosdede.expenseflow.address.mapper.AddressMapper;
+import dev.carlosdede.expenseflow.address.repository.AddressRepository;
+import dev.carlosdede.expenseflow.user.dto.UserCreateRequestDTO;
+import dev.carlosdede.expenseflow.user.dto.UserResponseDTO;
+import dev.carlosdede.expenseflow.user.entity.UserEntity;
+import dev.carlosdede.expenseflow.user.mapper.UserMapper;
 import dev.carlosdede.expenseflow.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper mapper;
 
-    public UserService(UserRepository userRepository){
+
+    public UserService(UserRepository userRepository, UserMapper mapper){
         this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    public ResponseEntity<UserResponseDTO> create(UserCreateRequestDTO userCreateRequestDTO){
+        if(userRepository.existsByEmail(userCreateRequestDTO.email())){
+           throw new RuntimeException("Email já está em uso");
+        }
+
+        UserEntity user = mapper.toEntity(userCreateRequestDTO);
+        UserEntity savedUser = userRepository.save(user);
+        return null;
     }
 
 
