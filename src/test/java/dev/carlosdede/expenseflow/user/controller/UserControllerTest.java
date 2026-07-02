d314fc7e7fedef07bc13ad444151eba640155e72
carlosdede <contato.carlosdede@gmail.com>
2026-07-02 18:50:21 -0300

    fix: injeção do ObjectMapper

--- src/test/java/dev/carlosdede/expenseflow/user/controller/UserControllerTest.java
+++ src/test/java/dev/carlosdede/expenseflow/user/controller/UserControllerTest.java
@@ -32,8 +32,7 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @MockitoBean
     private UserService userService;
